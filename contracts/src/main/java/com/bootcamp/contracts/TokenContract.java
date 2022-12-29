1cdabc4e0ce57396791ec632e68d6e7897b33d4c
Bruna Celestino <101028750+BrunaCelestino@users.noreply.github.com>
2022-12-29 15:49:01 -0300

    Update TokenContract.java

--- contracts/src/main/java/com/bootcamp/contracts/TokenContract.java
+++ contracts/src/main/java/com/bootcamp/contracts/TokenContract.java
@@ -12,12 +12,40 @@ import java.util.List;
 import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;
 import static net.corda.core.contracts.ContractsDSL.requireThat;
 
-public class TokenContract {
+public class TokenContract implements Contract{
     public static String ID = "com.bootcamp.contracts.TokenContract";
 
 
     public void verify(LedgerTransaction tx) throws IllegalArgumentException {
+        if(tx.getInputStates().size() != 0) {
+            throw new IllegalArgumentException("Inputs não são permitidos!");
+        }
+        if(tx.getOutputStates().size() !=1) {
+            throw new IllegalArgumentException("Deve haver apenas um output!");
+        }
+        if(tx.getCommands().size() !=1) {
+            throw new IllegalArgumentException("Deve haver um comando!");
+        }
 
+        if(!(tx.getOutput(0) instanceof TokenState)) {
+            throw new IllegalArgumentException("Output deve ser um tokenState!");
+        }
+
+        if(((TokenState) tx.getOutput(0)).getAmount() <=0) {
+            throw new IllegalArgumentException("Valor deve ser positivo!");
+        }
+
+        if(!(tx.getCommands().get(0).getValue() instanceof TokenContract.Commands.Issue)) {
+            throw new IllegalArgumentException("O comando deve ser um issue!");
+        }
+
+        if(!(tx.getCommands().get(0).getSigners().contains(((TokenState) tx.getOutput(0)).getIssuer().getOwningKey()))) {
+            throw new IllegalArgumentException("O Issuer deve assinar a transação!");
+        }
+
+        if(!(tx.getCommands().get(0).getSigners().contains(((TokenState) tx.getOutput(0)).getOwner().getOwningKey()))) {
+            throw new IllegalArgumentException("O Owner deve assinar a transação!");
+        }
     }
 
 
