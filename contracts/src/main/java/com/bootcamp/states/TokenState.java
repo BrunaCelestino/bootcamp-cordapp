4c8804a769ef35770863b4bf8146f125544b05ed
Bruna Celestino <101028750+BrunaCelestino@users.noreply.github.com>
2022-12-29 15:00:50 -0300

    Update TokenState.java

--- contracts/src/main/java/com/bootcamp/states/TokenState.java
+++ contracts/src/main/java/com/bootcamp/states/TokenState.java
@@ -13,6 +13,35 @@ import java.util.List;
 /* Our state, defining a shared fact on the ledger.
  * See src/main/java/examples/ArtState.java for an example. */
 @BelongsToContract(TokenContract.class)
-public class TokenState {
+public class TokenState implements ContractState{
+    private final Party issuer;
+    private final Party owner;
+    private final int amount;
 
+    public TokenState(Party issuer, Party owner, int amount) {
+        this.issuer = issuer;
+        this.owner = owner;
+        this.amount = amount;
+    }
+
+    public Party getIssuer() {
+        return issuer;
+    }
+
+    public Party getOwner() {
+        return owner;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @NotNull
+    @Override
+    public List<AbstractParty> getParticipants() {
+        List<AbstractParty> parties = new ArrayList<>();
+        parties.add(issuer);
+        parties.add(owner);
+        return parties;
+    }
 }
